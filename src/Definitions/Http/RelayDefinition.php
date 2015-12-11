b911d391044932de6d813f42d32cbe8838eb54cc
Maxime Fabre <ehtnam6@gmail.com>
2015-12-11 16:31:13 +0100

    Tweak RelayDefinition

--- src/Definitions/Http/RelayDefinition.php
+++ src/Definitions/Http/RelayDefinition.php
@@ -46,10 +46,8 @@ class RelayDefinition extends AbstractDefinitionProvider implements ImmutableCon
      */
     public function resolve($middleware)
     {
-        if (!$this->container && !is_string($middleware)) {
-            return $middleware;
-        } elseif (!$this->container && is_string($middleware)) {
-            return new $middleware();
+        if (!$this->container) {
+            return is_string($middleware) ? new $middleware : $middleware;
         }
 
         return $this->container->get($middleware);
